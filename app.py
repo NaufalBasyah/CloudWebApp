8e6abdffa8f6c2667f0c6b332fbd57430d4cdbf7
NaufalBasyah <Basyah_naufal@hotmail.com>
2020-12-27 20:05:29 +0700

    adding aboutUs page

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def index():
 def testss():
     return render_template('test.html', pageTitle="Test Page")
 
+@app.route('/aboutUs')
+def aboutUs():
+    return render_template('aboutUs.html', pageTitle="About Us Page")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
