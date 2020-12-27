8f292ebe9863893b1357f6d3c943629d94f0abdf
Ali <altaher99911@gmail.com>
2020-12-27 12:11:33 +0700

    firt initial use of jinja template

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('index.html', pageTitle="Home Page")
 
 @app.route('/test')
 def testss():
-    return "testing testing"
+    return render_template('test.html', pageTitle="Test Page")
 
 
 if __name__ == '__main__':
