347a8ad3226e9555cf9aa72f79c4a77f0bbac480
Stanshiki246 <stanleyorimura@gmail.com>
2021-01-01 20:54:56 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'halo-sayang'
+    host=os.environ.get('DB_HOST') or 'catalogue-db-246.cwzhix86ir4v.us-east-1.rds.amazonaws.com'
+    username=os.environ.get('DB_USERNAME') or 'psecloud5'
+    password=os.environ.get('DB_PASSWORD') or 'crimson7'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql+pymysql://' + username + ':' + password + '@' + host + '/users'
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
+        
+    
